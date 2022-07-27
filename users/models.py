5db2085f1a5aca8b3af72c20042b525bdcc360bd
lydiah1wachira <lydiahcedric@gmail.com>
2022-07-27 12:45:07 +0300

    create a templates folder.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    User model class to help create new user objects
+    '''
+    first_name = models.CharField(max_length=30)
+    bio = models
+    
